ce5fcbf07b4b1cd44e1c808463986020994d7ca6
thiagodks <thiagoadriano2010@gmail.com>
2021-04-12 11:52:50 -0300

    add methods: update speed, update_position

--- Particle.py
+++ Particle.py
@@ -7,9 +7,9 @@ class Particle:
 		self.ndim = ndim
 		self.xmin = xmin
 		self.xmax = xmax
-		self.speed = []
-		self.p_best = []
-		self.g_best = []
+		self.speed = [0] * ndim
+		self.p_best = [0] * ndim
+		self.g_best = [0] * ndim
 		self.coord = self.__initial_coord()
 		
 	def __initial_coord(self):
@@ -23,7 +23,24 @@ class Particle:
 		return (100*(x1 - (x2**2))**2 + (1 - x1)**2 + 90*(x4 - x3)**2 + (1 - x3)**2 +
 			    10.1*((x2 - 1)**2 + (x4 - 1)**2) + 19.8*(x2 - 1)*(x4 - 1))
 
+	def update_speed(self, W, C1, C2):
+		prev_speed = self.speed[-1]
+		r1, r2 = random.random(), random.random()
+
+		for i in range(0, self.ndim):
+			self.speed[i] = ((W * prev_speed) + (C1 * r1 * (self.p_best[i] - self.coord[i])) +
+							 (C2 * r2 * (self.g_best[i] - self.coord[i])))
+		
+	def update_position(self):
+		self.coord = [(self.coord[i] + self.speed[i]) for i in range(0, self.ndim)]
+		for i in range(0, self.ndim):
+			if self.coord[i] > self.xmax: self.coord[i] = self.xmax
+			elif self.coord[i] < self.xmin: self.coord[i] = self.xmin
 
 particle = Particle()
 particle.calc_fitness()
-print(particle.fitness)
+print(particle.fitness, particle.coord, particle.speed)
+particle.update_speed(W=0.5, C1=0.1, C2=0.1)
+particle.update_position()
+particle.calc_fitness()
+print("new values:", particle.fitness, particle.coord, particle.speed)
---
+++ main.py
@@ -0,0 +1,2 @@
+from Particle import Particle
+
