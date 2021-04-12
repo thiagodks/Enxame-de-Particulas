718c31683bea7bcaed68d0a156f2f3e58027d870
thiagodks <thiagoadriano2010@gmail.com>
2021-04-12 17:27:35 -0300

    update main.py: insert __main__, create parallel_exec to factorial tests

--- main.py
+++ main.py
@@ -4,30 +4,12 @@ import plot_charts as pc
 import numpy as  np
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-itr', required=False, default=100, type=int, help='Número de Iterações')
-parser.add_argument('-m', required=False, default=100, type=int, help='Número de Particulas')
-parser.add_argument('-w', required=False, default=0.05, type=float, help='Ponderamento da velocidade')
-parser.add_argument('-c1', required=False, default=0.01, type=float, help='Ponderamento p_best')
-parser.add_argument('-c2', required=False, default=0.01, type=float, help='Ponderamento g_best')
-parser.add_argument('-x1', required=False, default=-5, type=int, help='Valor de xmin')
-parser.add_argument('-x2', required=False, default=5, type=int, help='Valor de xmax')
-args = vars(parser.parse_args())
-
-MAX_ITR = int(args['itr'])
-M = int(args['m'])
-W = float(args['w'])
-C1 = float(args['c1'])
-C2 = float(args['c2'])
-XMIN = int(args['x1'])
-XMAX = int(args['x2'])
-
-def update_neighbors():
+def update_neighbors(particle_cloud, M):
     for i in range(0, M):
         possible_neighbors = particle_cloud[:i] + particle_cloud[i+1:]
         particle_cloud[i].set_neighbors(possible_neighbors)
 
-def generation_log():
+def generation_log(particle_cloud):
     particle_fitness = [p.fitness for p in particle_cloud]
     mean_fit = np.mean(particle_fitness)
     median_fit = np.median(particle_fitness)
@@ -35,51 +17,68 @@ def generation_log():
     best_solution = min(particle_cloud, key=lambda x: x.calc_fitness(x.g_best))
     return best_solution.calc_fitness(best_solution.g_best), mean_fit, median_fit, std_fit
 
-# Cria a nuvem de particulas
-particle_cloud = [Particle(xmin=XMIN, xmax=XMAX) for _ in range(0, M)]
-log_fitness = []
+if __name__ == "__main__":
 
-for itr in tqdm(range(0, MAX_ITR), position=0):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-itr', required=False, default=100, type=int, help='Número de Iterações')
+    parser.add_argument('-m', required=False, default=100, type=int, help='Número de Particulas')
+    parser.add_argument('-w', required=False, default=0.05, type=float, help='Ponderamento da velocidade')
+    parser.add_argument('-c1', required=False, default=0.01, type=float, help='Ponderamento p_best')
+    parser.add_argument('-c2', required=False, default=0.01, type=float, help='Ponderamento g_best')
+    parser.add_argument('-x1', required=False, default=-5, type=int, help='Valor de xmin')
+    parser.add_argument('-x2', required=False, default=5, type=int, help='Valor de xmax')
+    args = vars(parser.parse_args())
 
-    # Insere os vizinhos de cada particula
-    update_neighbors()
+    MAX_ITR = int(args['itr'])
+    M = int(args['m'])
+    W = float(args['w'])
+    C1 = float(args['c1'])
+    C2 = float(args['c2'])
+    XMIN = int(args['x1'])
+    XMAX = int(args['x2'])
 
-    for i, current_particle in enumerate(particle_cloud):
+    # Cria a nuvem de particulas
+    particle_cloud = [Particle(xmin=XMIN, xmax=XMAX) for _ in range(0, M)]
+    log_fitness = []
+
+    for itr in tqdm(range(0, MAX_ITR), position=0):
+
+        # Insere os vizinhos de cada particula
+        update_neighbors(particle_cloud, M)
+
+        for i, current_particle in enumerate(particle_cloud):
+            
+            # Calcula o fitness da particula atual
+            current_particle.update_fitness()
+            
+            # Atualiza p_best caso esse fitness seja melhor
+            if current_particle.fitness <= current_particle.calc_fitness(current_particle.p_best):
+                current_particle.p_best = current_particle.coord 
+
+                # Se atualizar p_best, pode ser que atualize tambem o g_best
+                if current_particle.fitness <= current_particle.calc_fitness(current_particle.g_best):
+                    current_particle.g_best = current_particle.coord
+
+            # Atualiza a velocidade da particula
+            current_particle.update_speed(W, C1, C2)
+
+            # Atualiza a posição da Particula de acordo com a nova velocidade
+            current_particle.update_position()
+            
+            particle_cloud[i] = current_particle
         
-        # Calcula o fitness da particula atual
-        current_particle.update_fitness()
-        
-        # Atualiza p_best caso esse fitness seja melhor
-        if current_particle.fitness <= current_particle.calc_fitness(current_particle.p_best):
-            current_particle.p_best = current_particle.coord 
-            # print("melhorou p_best: ", current_particle.p_best)
+        log_fitness.append(generation_log(particle_cloud))
 
-            # Se atualizar p_best, pode ser que atualize tambem o g_best
-            if current_particle.fitness <= current_particle.calc_fitness(current_particle.g_best):
-                current_particle.g_best = current_particle.coord
-                # print("melhorou g_best: ", current_particle.g_best)
-                # input("")
+    # Melhor solução encontrada!
+    best_solution = min(particle_cloud, key=lambda x: x.calc_fitness(x.g_best))
+    print("=> Best Solution: ", best_solution.calc_fitness(best_solution.g_best), best_solution.g_best)
 
-        # Atualiza a velocidade da particula
-        current_particle.update_speed(W, C1, C2)
+    parameters = { "MAX_ITR": MAX_ITR,
+                "M": M,
+                "W": W,
+                "C1": C1,
+                "C2": C2,
+                "XMIN": XMIN,
+                "XMAX": XMAX}
 
-        # Atualiza a posição da Particula de acordo com a nova velocidade
-        current_particle.update_position()
-        
-        particle_cloud[i] = current_particle
-    
-    log_fitness.append(generation_log())
-
-# Melhor solução encontrada!
-best_solution = min(particle_cloud, key=lambda x: x.calc_fitness(x.g_best))
-print("=> Best Solution: ", best_solution.calc_fitness(best_solution.g_best), best_solution.g_best)
-
-parameters = { "MAX_ITR": MAX_ITR,
-               "M": M,
-               "W": W,
-               "C1": C1,
-               "C2": C2,
-               "XMIN": XMIN,
-               "XMAX": XMAX}
-
-pc.plot_graphics(log_fitness, parameters)
+    pc.plot_graphics(log_fitness, parameters)
---
+++ parallel_exec.py
@@ -0,0 +1,68 @@
+from concurrent.futures import ProcessPoolExecutor
+from Particle import Particle
+from termcolor import colored
+from datetime import datetime
+from main import update_neighbors, generation_log
+import multiprocessing
+from tqdm import tqdm
+import itertools
+import pickle
+
+def run(prmt):
+    
+    p = {"MAX_ITR": prmt[0],
+        "M": prmt[1],
+        "W": prmt[2],
+        "C1": prmt[3],
+        "C2": prmt[4],
+        "XMIN": prmt[5],
+        "XMAX": prmt[6]}
+
+    particle_cloud = [Particle(xmin=p['XMIN'], xmax=p['XMAX']) for _ in range(0, p["M"])]
+    log_fitness = []
+
+    for _ in range(0, p["MAX_ITR"]):
+
+        # Insere os vizinhos de cada particula
+        update_neighbors(particle_cloud, p["M"])
+
+        for i, current_particle in enumerate(particle_cloud):
+            
+            # Calcula o fitness da particula atual
+            current_particle.update_fitness()
+            
+            # Atualiza p_best caso esse fitness seja melhor
+            if current_particle.fitness <= current_particle.calc_fitness(current_particle.p_best):
+                current_particle.p_best = current_particle.coord 
+
+                # Se atualizar p_best, pode ser que atualize tambem o g_best
+                if current_particle.fitness <= current_particle.calc_fitness(current_particle.g_best):
+                    current_particle.g_best = current_particle.coord
+
+            # Atualiza a velocidade da particula
+            current_particle.update_speed(p["W"], p["C1"], p["C2"])
+
+            # Atualiza a posição da Particula de acordo com a nova velocidade
+            current_particle.update_position()
+            
+            particle_cloud[i] = current_particle
+        
+        log_fitness.append(generation_log(particle_cloud))
+
+MAX_ITR = [50, 100, 150, 200, 250]
+M = [100, 200, 300, 400, 500]
+W = [0.1, 0.15, 0.2]
+C1 = [0.1, 0.15, 0.2]
+C2 = [0.1, 0.15, 0.2]
+XMIN = [10]
+XMAX = [10]
+
+all_list = [MAX_ITR, M, W, C1, C2, XMIN, XMAX]
+parameters = list(itertools.product(*all_list)) 
+
+executor = ProcessPoolExecutor()
+num_args = len(parameters)
+chunksize = int(num_args/multiprocessing.cpu_count())
+results = [i for i in tqdm(executor.map(run, parameters),total=num_args)]
+
+pickle.dump(results, open("results_"+len(parameters)+"_.pickle", "wb"))
