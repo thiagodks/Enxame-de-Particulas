fe4e55b747eab0fbf9c227de309dfc0538ae789d
thiagodks <thiagoadriano2010@gmail.com>
2021-04-12 23:58:47 -0300

    finish, new results

--- plot_charts.py
+++ plot_charts.py
@@ -5,6 +5,14 @@ import pandas as pd
 from datetime import datetime
 import argparse
 
+def get_fitness(fitness):
+	x1 = str(fitness)
+	if x1.find("e") == -1: 
+		return ("%.6f" % fitness)
+	x2 = x1[:6]
+	x2 += x1[x1.find("e"):]
+	return x2
+
 def plot_graphics(gen_log, args, name=""):
 
 	gen_log = list(map(list, zip(*gen_log)))
@@ -20,16 +28,16 @@ def plot_graphics(gen_log, args, name=""):
 	ax1.set_title("Fitness a cada Iteração")
 	ax1.set_xlabel("Iterações", fontsize='medium')
 	ax1.set_ylabel("Fitness", fontsize='medium')
-	ax1.plot(list(range(0, itr)), p_best, 'g--', label='Melhor Fitness: %.4f' % p_best[-1])
+	ax1.plot(list(range(0, itr)), p_best, 'g--', label='Melhor Fitness: ' + get_fitness(p_best[-1]))
 	ax1.legend(ncol=1)
 	ax1.tick_params(labelsize=18)
 
 	ax2.set_title("Media e Mediana da fitness")
 	ax2.set_xlabel("Iterações", fontsize='medium')
 	ax2.set_ylabel("Fitness", fontsize='medium')
-	ax2.plot(list(range(0, itr)), p_best, 'g--', label='Melhor Fitness: %.4f' % (p_best[-1]))
-	ax2.plot(list(range(0, itr)), mean_fitness, 'b--', label='Media: %.4f' % (mean_fitness[-1]))
-	ax2.plot(list(range(0, itr)), median_fitness, 'y--', label='Mediana: %.4f' % (median_fitness[-1]))
+	ax2.plot(list(range(0, itr)), p_best, 'g--', label='Melhor Fitness: ' + get_fitness(p_best[-1]))
+	ax2.plot(list(range(0, itr)), mean_fitness, 'b--', label='Media: ' + get_fitness(mean_fitness[-1]))
+	ax2.plot(list(range(0, itr)), median_fitness, 'y--', label='Mediana: ' + get_fitness(median_fitness[-1]))
 	ax2.legend(ncol=1)
 	ax2.tick_params(labelsize=18)
 
@@ -51,8 +59,6 @@ def plot_table(results):
 			"$\\bf{STD}$": []}
 
 	for i in results:
-		# print(i)
-		# input("")
 		cs = min(i[0], key=lambda x: x[0])
 		table["$\\bf{ITR}$"].append(i[1]["MAX_ITR"])
 		table["$\\bf{M}$"].append(i[1]["M"])
@@ -61,10 +67,10 @@ def plot_table(results):
 		table["$\\bf{C2}$"].append(i[1]["C2"])
 		table["$\\bf{XMIN}$"].append(i[1]["XMIN"])
 		table["$\\bf{XMAX}$"].append(i[1]["XMAX"])
-		table["$\\bf{FIT}$"].append("%.3f" % cs[0])
-		table["$\\bf{ME}$"].append("%.2f" % cs[1])
-		table["$\\bf{MD}$"].append("%.2f" % cs[2])
-		table["$\\bf{STD}$"].append("%.2f" % cs[3])
+		table["$\\bf{FIT}$"].append(get_fitness(cs[0]))
+		table["$\\bf{ME}$"].append(get_fitness(cs[1]))
+		table["$\\bf{MD}$"].append(get_fitness(cs[2]))
+		table["$\\bf{STD}$"].append(get_fitness(cs[3]))
 
 	df = pd.DataFrame(data=table)
 
@@ -73,14 +79,14 @@ def plot_table(results):
 	fig.patch.set_visible(False)
 	plt.axis("off")
 	plt.grid("off")
-	fig.set_size_inches(4, 2)
-	ax.set_title("Top-20 Execuções", y=1.65, fontdict={"fontsize": 6}, weight='bold')
+	# fig.set_size_inches(4, 2)
+	ax.set_title("Top-20 Execuções", y=0.99, fontdict={"fontsize": 6}, weight='bold')
 
 	ncol = len(table.keys())
 	colors = [["#ccccb3"] * ncol, ["#e0e0d1"] * ncol] * int(len(results)/2)
 	the_table = ax.table(cellText=df.values,colLabels=df.columns, cellColours=colors, cellLoc="center", loc="center", colColours =["#78786d"] * ncol)
 	the_table.auto_set_font_size(False)
-	the_table.set_fontsize(5)
+	the_table.set_fontsize(6)
 
 	fig.tight_layout()
 	now = datetime.now()
@@ -95,8 +101,6 @@ if __name__ == "__main__":
 	args = vars(parser.parse_args())
 	results = pickle.load(open(args['f'], 'rb'))
 	results.sort(key=lambda x: min(x[0], key=lambda x: x[0]))
-	# for r in results:
-		# print(r[0][0][-1], r[0][1][-1], r[0][2][-1], r[0][3][-1])
-		# print(min(r[0], key=lambda x: x[0]))
-		# input("")
 	plot_table(results[:20])
+	plot_graphics(results[0][0], results[0][1], "best")
+	plot_graphics(results[-1][0], results[-1][1], "worst")
